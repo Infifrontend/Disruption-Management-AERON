890e58fb670fa272e2839e70db505edd4819b9cd
Infifrontend <infifrontend@gmail.com>
2025-08-25 05:17:49 +0000

    Configuration changes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -49,7 +49,7 @@ const iconMap = {
   Settings,
 };
 
-const flydubaiLogo = `${import.meta.env.VITE_FRONTEND_BASE_URL}/public/flydubai_logo.png`;
+const flydubaiLogo = `/public/flydubai_logo.png`;
 
 interface LayoutProps {
   children: ReactNode;
