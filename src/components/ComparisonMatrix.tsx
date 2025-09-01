146e13aa129119782afa26a4de0abb71a5ba0e97
Infifrontend <infifrontend@gmail.com>
2025-09-01 10:55:09 +0000

    Restored to '2315468cb02ed5db8a3cb442215cc20108d03072'
    Replit-Restored-To: 2315468cb02ed5db8a3cb442215cc20108d03072

--- src/components/ComparisonMatrix.tsx
+++ src/components/ComparisonMatrix.tsx
@@ -3102,9 +3102,6 @@ export function ComparisonMatrix({
               </div>
             </div>
           </div>
-        )}
-      </DialogContent>
-    </Dialog>
         </DialogContent>
       </Dialog>
 
